b421614a4f0267da8fb87c604f62bb6e289565be
_ikro_ <icaroclash77@gmail.com>
2025-09-07 22:12:14 -0300

    Criando pacotes

---
+++ src/main/java/uninter/tarefas/model/Tarefas.java
@@ -0,0 +1,9 @@
+package uninter.tarefas.model;
+
+import java.time.LocalDate;
+
+public class Tarefas {
+    private String nome;
+    private LocalDate dataEntrega;
+    private String responsavel;
+}
